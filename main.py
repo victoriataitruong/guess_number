32aa84aea98adf4a242062ba15e16b3af6a549bb
Victoria Truong <victoriataitruong@gmail.com>
2021-06-13 20:22:57 -0700

    updating comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# import libraries
 import random
 number = random.randint(1, 10)
 
+# asking user for input
 player_name = input("Hello, What's your name?")
 number_of_guesses = 0
 print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
